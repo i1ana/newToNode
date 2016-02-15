8646484e2cd1c63ea8f93b0259bf546e6462a429
Ilana <bachwasbaroque@gmail.com>
2016-02-15 15:17:59 -0700

    part 2 done

--- luminosity-cli.js
+++ luminosity-cli.js
@@ -1,4 +1,11 @@
 // Requires luminosity 
 
-var export = require('./colorutil')
+var colorutil = require('./colorutil')
 
+var r = process.argv[2];
+var g = process.argv[3];
+var b = process.argv[4];
+
+var luminosity = colorutil.luminosity(r, g, b)
+
+console.log(luminosity)
